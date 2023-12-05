dc8dac4c79e98598a21c98841c4265eee6818cf6
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2023-12-05 18:28:04 +0200

    add ApplicationDbContext and TopicTagConfiguration, fix TopicConfiguration...

--- CodeForum/Configurations/TopicConfiguration.cs
+++ CodeForum/Configurations/TopicConfiguration.cs
@@ -4,18 +4,32 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace CodeForum.Configurations;
 
-public class TopicTagConfiguration : IEntityTypeConfiguration<TopicTag>
+public class TopicConfiguration : IEntityTypeConfiguration<Topic>
 {
-    public void Configure(EntityTypeBuilder<TopicTag> builder)
+    public void Configure(EntityTypeBuilder<Topic> builder)
     {
-        builder.HasKey(tt => new { tt.TopicId, tt.TagId });
+        builder.HasOne(topic => topic.User)
+            .WithMany(user => user.Topics)
+            .HasForeignKey(topic => topic.UserId);
 
-        builder.HasOne(tt => tt.Topic)
-            .WithMany(topic => topic.TopicTags)
+        builder.HasOne(topic => topic.Category)
+            .WithMany(category => category.Topics)
+            .HasForeignKey(topic => topic.CategoryId);
+
+        builder.HasMany(topic => topic.Posts)
+            .WithOne(post => post.Topic)
+            .HasForeignKey(post => post.TopicId);
+
+        builder.HasMany(topic => topic.Ratings)
+            .WithOne(rating => rating.Topic)
+            .HasForeignKey(rating => rating.TopicId);
+
+        builder.HasMany(topic => topic.Favorites)
+            .WithOne(favorite => favorite.Topic)
+            .HasForeignKey(favorite => favorite.TopicId);
+
+        builder.HasMany(topic => topic.TopicTags)
+            .WithOne(tt => tt.Topic)
             .HasForeignKey(tt => tt.TopicId);
-
-        builder.HasOne(tt => tt.Tag)
-            .WithMany(tag => tag.TopicTags)
-            .HasForeignKey(tt => tt.TagId);
     }
 }
---
+++ CodeForum/Context/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using CodeForum.Configurations;
+using CodeForum.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeForum.Context;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Topic> Topics { get; set; }
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Rating> Ratings { get; set; }
+    public DbSet<Favorite> Favorites { get; set; }
+    public DbSet<Report> Reports { get; set; }
+    public DbSet<TopicTag> TopicTags { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+    public DbSet<Notification> Notifications { get; set; }
+    public DbSet<LikeDislike> LikesDislikes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfiguration(new ApplicationUserConfiguration());
+        modelBuilder.ApplyConfiguration(new TopicConfiguration());
+        modelBuilder.ApplyConfiguration(new PostConfiguration());
+        modelBuilder.ApplyConfiguration(new RatingConfiguration());
+        modelBuilder.ApplyConfiguration(new FavoriteConfiguration());
+        modelBuilder.ApplyConfiguration(new ReportConfiguration());
+        modelBuilder.ApplyConfiguration(new TopicTagConfiguration());
+    }
+}
