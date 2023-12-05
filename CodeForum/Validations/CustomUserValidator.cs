3369ed588aa94593e99459b67d0193ba11aef0ca
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2023-12-05 18:39:40 +0200

    add Validations...

---
+++ CodeForum/Validations/CustomUserValidator.cs
@@ -0,0 +1,29 @@
+﻿using CodeForum.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace CodeForum.Validations;
+
+public class CustomUserValidator : IUserValidator<ApplicationUser>
+{
+    public Task<IdentityResult> ValidateAsync(UserManager<ApplicationUser> manager, ApplicationUser user)
+    {
+        List<IdentityError> errors = new List<IdentityError>();
+ 
+        if (user.Email.ToLower().EndsWith("@spam.com"))
+        {
+            errors.Add(new IdentityError
+            {
+                Description = "Choose another mail service"
+            });
+        }
+        if (user.UserName.Contains("admin"))
+        {
+            errors.Add(new IdentityError
+            {
+                Description = "Not contain the word 'admin'!!!"
+            });
+        }
+        return Task.FromResult(errors.Count == 0 ?
+            IdentityResult.Success : IdentityResult.Failed(errors.ToArray()));
+    }
+}
---
+++ CodeForum/Validations/FileTypeValidationAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodeForum.Validations;
+
+public class FileTypeValidationAttribute : ValidationAttribute
+{
+    private readonly string[] _validTypes;
+
+    public FileTypeValidationAttribute(params string[] validTypes)
+    {
+        _validTypes = validTypes;
+    }
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        if (value is IFormFile file)
+        {
+            var fileExtension = Path.GetExtension(file.Name).ToLowerInvariant();
+            if (!_validTypes.Any(validType => fileExtension.EndsWith(validType)))
+            {
+                return new ValidationResult($"File type must be one of: {string.Join(", ", _validTypes)}");
+            }
+        }
+        return ValidationResult.Success;
+    }
+}
