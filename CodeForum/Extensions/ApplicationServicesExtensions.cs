db01d5bbe379b4eb6ac0f6168cfe3d952863924f
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2023-12-05 18:43:45 +0200

    add ApplicationServicesExtensions

---
+++ CodeForum/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,42 @@
+﻿using CodeForum.Context;
+using CodeForum.Models;
+using CodeForum.Validations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeForum.Extensions;
+
+public static class ApplicationServicesExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+        IConfiguration config)
+    {
+        services.AddTransient<IUserValidator<ApplicationUser>, CustomUserValidator>();
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+        });
+
+        services.AddIdentity<ApplicationUser, IdentityRole>(opts =>
+            {
+                opts.User.RequireUniqueEmail = true;
+                opts.User.AllowedUserNameCharacters = ".@abcdefghijklmnopqrstuvwxyz";
+                opts.Password.RequiredLength = 5;
+                opts.Password.RequireNonAlphanumeric = false;
+                opts.Password.RequireLowercase = false;
+                opts.Password.RequireUppercase = false;
+                opts.Password.RequireDigit = false;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>().AddRoles<IdentityRole>();
+
+        services.Configure<RequestLocalizationOptions>(options =>
+        {
+            options.DefaultRequestCulture = new RequestCulture("en-US");
+        });
+
+
+        return services;
+    }
+}
--- CodeForum/Program.cs
+++ CodeForum/Program.cs
@@ -1,7 +1,11 @@
+using CodeForum.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
