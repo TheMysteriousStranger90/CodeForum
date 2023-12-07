e8159575efa7230fef6c90820d5b61d3ed24ba1b
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2023-12-07 13:26:32 +0200

    fix...

---
+++ CodeForum/ViewComponents/CategoryListViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using CodeForum.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeForum.ViewComponents;
+
+public class CategoryListViewComponent : ViewComponent
+{
+    private readonly ICategoryRepository _categoryRepository;
+
+    public CategoryListViewComponent(ICategoryRepository categoryRepository)
+    {
+        _categoryRepository = categoryRepository;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var categories = await _categoryRepository.ListAllAsync();
+        return View(categories);
+    }
+}
